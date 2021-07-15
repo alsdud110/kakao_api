c2b0825dc5553d54eacaf2ff6c4f397c1eecd2e9
황민영 <alsdud110@naver.com>
2021-07-15 09:53:21 +0900

    Test

---
+++ index.js
@@ -0,0 +1,12 @@
+var person = {
+    'first-name' : 'Ung-mo',
+    'last-name' : 'Lee',
+    'gender' : 'male',
+    1: 10
+};
+
+console.log(person);
+
+console.log(person.gender);
+console.log(person["first-name"]);
+console.log(person.first-name);
